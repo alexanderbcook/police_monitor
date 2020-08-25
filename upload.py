9a4a5751e0fec1a167ebcd7fafe7f309301b5613
alexanderbcook <alex.chef.cook@gmail.com>
2020-08-25 00:40:57 +0000

    remove print statements, set connnection string correctly

--- upload.py
+++ upload.py
@@ -12,7 +12,7 @@ import pytz
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 try:
-    conn = psycopg2.connect("dbname='postgres' host ='localhost'")
+    conn = psycopg2.connect("dbname='postgres'")
 except:
     print("Can't connect to PSQL!")
 try:
@@ -43,8 +43,7 @@ while i > 0:
 
     pst_datetime = utc_datetime.astimezone(pytz.timezone("America/Los_Angeles"))
     pst_date = datetime.strftime(pst_datetime, '%Y-%m-%d %I:%M %p')
-    print(pst_date)
-    print(str(pst_date))
+
     at = 'at'
     startBracket = '['
 
