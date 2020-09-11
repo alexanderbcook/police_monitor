dfb29048267d3a63888989830db40df2718d5ba9
Alex Cook <alex.chef.cook@gmail.com>
2020-09-10 17:45:32 -0700

    update code to use thread/queuing

--- listener.py
+++ listener.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python
-
-#USAGE: python listener.py -q portland -m d
-
 import tweepy
 import config
 import redis
 import json
 import os
 from json import dumps
-from util import encode, connect_to_api, json_serial
+from util import connect_to_api, json_serial
+from queue import Queue
+from threading import Thread
 
 redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-accountId = ['1602852614']
-
-count = 0
+accountId = ['1602852614', '1606472113']
 
 class StreamListener(tweepy.StreamListener):
     
-    def on_status(self, data):
+    def __init__(self, queue = Queue()):
+        number_worker_threads = 4
+        self.queue = queue
+        for i in range(number_worker_threads):
+            thread = Thread(target=self.upload_tweet)
+            thread.daemon = True
+            thread.start()
+
+    def on_data(self, data):
+
+        jsonData = json.loads(data)
 
         tweet = {}
-        tweet['id'] = data.id_str
-        tweet['created_at'] = dumps(data.created_at, default=json_serial)
-        tweet['text'] = data.text
-        tweet['username'] = data.user.screen_name
-        tweet['url'] = data.user.url
-        tweet['location'] = data.user.location
+        tweet['id'] = jsonData['id_str']
+        tweet['text'] = jsonData['text']
+        tweet['username'] = jsonData['user']['screen_name']
+        tweet['url'] = jsonData['user']['url']
+        tweet['location'] = jsonData['user']['location']
         json_tweet = json.dumps(tweet)
         
-        redis.lpush('police', json_tweet)
+        self.queue.put(redis.lpush('police', json_tweet))
 
-        os.system('python upload.py -q police')
+    def upload_tweet(self):
+        while True:
+            self.queue.get()
+            os.system('python upload.py -q police')
+            self.queue.task_done()
 
 if __name__ == '__main__':
     api = connect_to_api(config)
--- upload.py
+++ upload.py
@@ -1,7 +1,6 @@
 import redis
 import json
 import argparse
-import logging
 import psycopg2
 from psycopg2 import *
 from datetime import datetime
@@ -11,7 +10,6 @@ import geocode
 from geocode import geocode
 
 
-logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 try:
     conn = psycopg2.connect("dbname='postgres'")
@@ -33,6 +31,7 @@ args = parser.parse_args()
 
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS twitter.police (id BIGINT, createdate TIMESTAMP, body VARCHAR, username VARCHAR, url VARCHAR, location VARCHAR, address VARCHAR, neighborhood VARCHAR, city VARCHAR, zipcode VARCHAR, lat NUMERIC, lng NUMERIC, incident_type VARCHAR, urgency VARCHAR)")
+cur.execute("CREATE TABLE IF NOT EXISTS twitter.fire (id BIGINT, createdate TIMESTAMP, body VARCHAR, username VARCHAR, url VARCHAR, location VARCHAR, address VARCHAR, neighborhood VARCHAR, city VARCHAR, zipcode VARCHAR, lat NUMERIC, lng NUMERIC, incident_type VARCHAR, urgency VARCHAR)")
 
 i = redis.llen(args.query)
 
@@ -46,14 +45,6 @@ while i > 0:
         isRetweet = True
 
     if isRetweet != True:
-
-        # UTC to PST conversion.
-        datetime_object = datetime.strptime(json.loads(data['created_at']), '%Y-%m-%dT%H:%M:%S')
-
-        utc_datetime = pytz.utc.localize(datetime_object)
-
-        pst_datetime = utc_datetime.astimezone(pytz.timezone("America/Los_Angeles"))
-        pst_datestring = datetime.strftime(pst_datetime, '%Y-%m-%d %I:%M %p')
 
         # Tweets are usually formatted like this ROBBERY - COLD at 1200 SW ALDER ST, PORT [id]
         # So everything prior to the 'at' are the incident details, everything between the 'at' and the '[' are the incident details.
@@ -78,13 +69,19 @@ while i > 0:
         # If geocode is successful, upload latitude, longitude, neighborhood and zip code information. Otherwise, just upload the base data.
         
         geocodeJson = geocode(address)
-        print(geocodeJson)
-        try:
-            cur.execute("INSERT INTO twitter.police (id, createdate, body, username, url, location, address, neighborhood, city, zipcode, lat, lng, incident_type, urgency) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (str(data['id']), str(pst_datestring), str(data['text']), str(data['username']), str(data['url']), str(data['location']), str(address), geocodeJson['raw']['neighborhood'], geocodeJson['city'], geocodeJson['postal'], geocodeJson['lat'], geocodeJson['lng'], str(incidentType), str(urgency)))
-        except:
-            cur.execute("INSERT INTO twitter.police (id, createdate, body, username, url, location, address, neighborhood, city, zipcode, lat, lng, incident_type, urgency) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (str(data['id']), str(pst_datestring), str(data['text']), str(data['username']), str(data['url']), str(data['location']), str(address), '','', '', None, None, str(incidentType), str(urgency)))
+        if str(data['username']) == 'pdxpolicelog':
+            try:
+                cur.execute("INSERT INTO twitter.police (id, createdate, body, username, url, location, address, neighborhood, city, zipcode, lat, lng, incident_type, urgency) VALUES (%s, NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (str(data['id']), str(data['text']), str(data['username']), str(data['url']), str(data['location']), str(address), geocodeJson['raw']['neighborhood'], geocodeJson['city'], geocodeJson['postal'], geocodeJson['lat'], geocodeJson['lng'], str(incidentType), str(urgency)))
+            except:
+                cur.execute("INSERT INTO twitter.police (id, createdate, body, username, url, location, address, neighborhood, city, zipcode, lat, lng, incident_type, urgency) VALUES (%s, NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (str(data['id']), str(data['text']), str(data['username']), str(data['url']), str(data['location']), str(address), '','', '', None, None, str(incidentType), str(urgency)))
 
-        
+        if str(data['username']) == 'pdxfirelog':
+            try:
+                cur.execute("INSERT INTO twitter.fire (id, createdate, body, username, url, location, address, neighborhood, city, zipcode, lat, lng, incident_type, urgency) VALUES (%s, NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (str(data['id']), str(data['text']), str(data['username']), str(data['url']), str(data['location']), str(address), geocodeJson['raw']['neighborhood'], geocodeJson['city'], geocodeJson['postal'], geocodeJson['lat'], geocodeJson['lng'], str(incidentType), str(urgency)))
+            except:
+                cur.execute("INSERT INTO twitter.fire (id, createdate, body, username, url, location, address, neighborhood, city, zipcode, lat, lng, incident_type, urgency) VALUES (%s, NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (str(data['id']), str(data['text']), str(data['username']), str(data['url']), str(data['location']), str(address), '','', '', None, None, str(incidentType), str(urgency)))
+
+
     i = i - 1
 
 conn.commit()
